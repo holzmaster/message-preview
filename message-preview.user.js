afc237bcdef5ca03e84dd5029bb7f730a8b824d3
holzmaster <holzmaster@outlook.com>
2021-11-24 00:59:14 +0100

    Bump version

--- message-preview.user.js
+++ message-preview.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        Nachrichtenvorschau
-// @version     1.1.1
+// @version     1.1.2
 // @author      holzmaster
 // @namespace   holzmaster
 // @include     https://pr0gramm.com*
